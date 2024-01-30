5b2c0e146fa639b8ae6c986266140306a1412022
zeinabmallaki67 <zeinabmallaki@gmail.com>
2024-01-30 20:16:43 +0330

    edit project

--- app/src/main/java/com/zeinabmallaki/sendsmsanic/MainActivity.kt
+++ app/src/main/java/com/zeinabmallaki/sendsmsanic/MainActivity.kt
@@ -1,47 +1,65 @@
 package com.zeinabmallaki.sendsmsanic
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.net.Uri
+import android.Manifest
+import android.content.pm.PackageManager
+import android.telephony.SmsManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.zeinabmallaki.sendsmsanic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.sendButton.setOnClickListener {
-            val phoneNumber = binding.phoneNumber.text.toString()
-            val message = binding.message.text.toString()
+        binding.sendButton.setOnClickListener { sendMessage() }
+    }
 
-            if (phoneNumber.isNotEmpty() && message.isNotEmpty()) {
-                sendSMS(phoneNumber, message)
+    private fun sendMessage() {
+        val mobileNumber = binding.phoneNumber.text.toString()
+        val message = binding.message.text.toString()
+
+        if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+            sendSMS(mobileNumber, message)
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.SEND_SMS), PERMISSION_REQUEST_SEND_SMS)
+        }
+    }
+
+    private fun sendSMS(mobileNumber: String, message: String) {
+        try {
+            val smsManager: SmsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(mobileNumber, null, message, null, null)
+            Toast.makeText(this, "SMS sent successfully.", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(this, "Failed to send SMS.", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_SEND_SMS) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val mobileNumber = binding.phoneNumber.text.toString()
+                val message = binding.message.text.toString()
+                sendSMS(mobileNumber, message)
+            } else {
+                Toast.makeText(this, "Permission denied to send SMS.", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun sendSMS(phoneNumber: String, message: String) {
-        val smsUri = Uri.parse("sms:$phoneNumber")
-        val intent = Intent(Intent.ACTION_SENDTO, smsUri)
-        intent.putExtra("sms_body", message)
-        try {
-            startActivity(intent)
-        } catch (e: ActivityNotFoundException) {
-            toast("Error: SMS app not found")
-        }
-    }
-
-
-    private fun toast(text: String) {
-        Toast.makeText(
-            this,
-            text,
-            Toast.LENGTH_SHORT
-        ).show()
+    companion object {
+        private const val PERMISSION_REQUEST_SEND_SMS = 123
     }
 }
